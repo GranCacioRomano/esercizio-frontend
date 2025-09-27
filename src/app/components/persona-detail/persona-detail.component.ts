8e8e9531546f5e4680c5c013d9962af75dfb3686
GranCacioRomano <falconetti.grafico@gmail.com>
2025-09-27 18:00:27 +0200

    Creazione dei componenti Navbar, Persone, Persona Detail

---
+++ src/app/components/persona-detail/persona-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persona-detail',
+  imports: [],
+  templateUrl: './persona-detail.component.html',
+  styleUrl: './persona-detail.component.scss'
+})
+export class PersonaDetailComponent {
+
+}
---
+++ src/app/components/persona-list/persona-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persona-list',
+  imports: [],
+  templateUrl: './persona-list.component.html',
+  styleUrl: './persona-list.component.scss'
+})
+export class PersonaListComponent {
+
+}
