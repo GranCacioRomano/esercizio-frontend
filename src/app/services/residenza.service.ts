b00799fd3771b6fa980ff56bbf99a6c07cab098c
GranCacioRomano <falconetti.grafico@gmail.com>
2025-09-27 18:16:01 +0200

    Correzione Service di Persona e implementazione del Service di Residenza

--- src/app/services/residenza.service.ts
+++ src/app/services/residenza.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Residenza } from '../models/residenza.model';
+import { Persona } from '../models/persona.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResidenzaService {
+  private apiUrl = 'http://localhost:8080/api/v1/residenza';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Residenza[]> {
+    return this.http.get<Residenza[]>(this.apiUrl);
+  }
+  getById(id: number): Observable<Residenza> {
+    return this.http.get<Residenza>(`${this.apiUrl}/${id}`);
+  }
+  create(residenza: Residenza): Observable<Residenza> {
+    return this.http.post<Residenza>(this.apiUrl, residenza);
+  }
+  update(id: number, residenza: Residenza): Observable<Residenza> {
+    return this.http.put<Residenza>(`${this.apiUrl}/${id}`, residenza);
+  }
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+  getPersoneByIndirizzo(indirizzo: string): Observable<Persona[]> {
+    return this.http.get<Persona[]>(`${this.apiUrl}/persone?indirizzo=${indirizzo}`);
+  }
 }
