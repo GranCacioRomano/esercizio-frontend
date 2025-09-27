0c82c45725ed0680937fa4f4335ee4aa4b5fa00e
GranCacioRomano <falconetti.grafico@gmail.com>
2025-09-27 17:58:46 +0200

    Aggiunte le rotte per Persona

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PersonaListComponent } from './components/persona-list/persona-list.component';
+import { PersonaDetailComponent } from './components/persona-detail/persona-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'persone', component: PersonaListComponent},
+  {path: 'persone/:id', component: PersonaDetailComponent},
+  {path: '', redirectTo: 'persone', pathMatch: 'full' }
+];
